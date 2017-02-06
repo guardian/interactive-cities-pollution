c42f38363e35bbe831ed4f693a3bdab799d48504
cfen <chris.fenn@guardian.co.uk>
2017-02-06 12:03:15 +0000

    Added social buttons

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,11 @@ import findUser from '../lib/findUser'
 import riskSummaryChart from '../lib/riskSummaryChart'
 import riskByTypeCharts from '../lib/riskByTypeCharts'
 import cities_csv from '../data/pollution_geolocated.csv'
-
+import { share } from '../lib/share.js'
 
 let summaryChart;
+
+var shareFn = share('Share text goes here','https://www.theguardian.com/p/5dtd9')
 
 function init(){
 
@@ -29,6 +31,13 @@ function init(){
 	let riskMultiples = riskByTypeCharts(cities);
 	riskMultiples.init();
 	userControls.registerListener(riskMultiples.setUserData);
+
+
+	// Share buttons
+	[].slice.apply(document.querySelectorAll('.gv-share-container button')).forEach(shareEl => {
+	    var network = shareEl.className.replace('share-','');
+	    shareEl.addEventListener('click',() => shareFn(network));
+	});
 
 
 }
