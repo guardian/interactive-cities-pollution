2c69b3f39287416b250a8718b96208dc9d8186b5
Feilding Cage <feilding.cage@gmail.com>
2017-02-07 08:30:13 +0000

    merges with google doc for copy

--- src/render.js
+++ src/render.js
@@ -1,5 +1,13 @@
 import mainTemplate from './src/templates/main.html!text'
+import Mustache from 'mustache'
+import rp from 'request-promise'
 
-export async function render() {
-    return mainTemplate;
+export function render() {
+    return rp({
+        uri: 'https://interactive.guim.co.uk/docsdata-test/1or5QZTnVlJqAwd2EOp4gPJWR4s5OgrEKD2A5EuQKoB0.json',
+        json: true
+    }).then((data) => {
+        var html = Mustache.render(mainTemplate, data);
+        return html;
+    });
 }
